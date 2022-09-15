4b06be54f6cad0d2c8f394b31b62cb26f0292415
tom1484 <angelus0918866416@gmail.com>
2022-09-15 17:33:08 +0800

    Update

--- hw2/main.js
+++ hw2/main.js
@@ -5,7 +5,7 @@ window.onload = () => {
     let userPin = document.getElementsByClassName("user-pin-area")[0];
     let userList = document.getElementsByClassName("user-list")[0];
 
-    let templateUser = document.getElementById("template-user");
+    let templateUser = document.querySelector("template").content.querySelector("#template-user");
     let defaultUserInfo = [
         { userName: "User0", userImage: "./css/img/user/user-pin.png", isSelf: true }, 
         { userName: "User1", userImage: "./css/img/user/user-list-1.png", isSelf: false }, 
@@ -14,6 +14,9 @@ window.onload = () => {
         { userName: "User4", userImage: "./css/img/user/user-list-4.png", isSelf: false }, 
         { userName: "User5", userImage: "./css/img/user/user-list-5.png", isSelf: false }, 
     ];
+
+    let participantCountTag = document.getElementsByClassName("participant-count-text")[0];
+    participantCountTag.innerText = defaultUserInfo.length;
 
     for (let userInfo of defaultUserInfo) {
         let userTag = templateUser.cloneNode(true);
@@ -28,6 +31,21 @@ window.onload = () => {
         // set user image
         let userImageTag = userTag.getElementsByClassName("user-img")[0];
         userImageTag.setAttribute("src", userInfo.userImage);
+        // set delete button event listener for clicking
+        let deleteButton = userTag.getElementsByClassName("delete-user")[0];
+        let deleteClickListener = () => {
+            if (userTag.getAttribute("pinned") == "true") {
+                // unpin user
+                userPin.setAttribute("enabled", false);
+
+                userPin.removeChild(userTag);
+            }
+            else {
+                userList.removeChild(userTag);
+            }
+            participantCountTag.innerText -= 1;
+        }
+        deleteButton.addEventListener("click", deleteClickListener);
         // set tooltip event listener for clicking
         let tooltipOptions = userTag.getElementsByClassName("tooltip-option");
         let pinClickListener = () => {
