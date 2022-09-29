61110229e6bdd8b9416eb448871db5a99836ee4e
tom1484 <angelus0918866416@gmail.com>
2022-09-29 16:27:20 +0800

    Update

--- hw2/main.js
+++ hw2/main.js
@@ -1,7 +1,8 @@
 window.onload = () => {
 
     let userPin = document.getElementsByClassName("user-pin-area")[0];
-    let userList = document.getElementsByClassName("user-list")[0];
+    // let userListArea = document.getElementsByClassName("user-list-area")[0];
+    let userList = document.getElementsByClassName("user-list-area")[0];
 
     let templateUser = document.querySelector("template").content.querySelector("#template-user");
     let defaultUserInfo = [
@@ -13,8 +14,103 @@ window.onload = () => {
         { userName: "User5", userImage: "./css/img/user/user-list-5.png", isSelf: false }, 
     ];
 
+    let listUserCount = 0;
     let participantCountTag = document.getElementsByClassName("participant-count-text")[0];
     participantCountTag.innerText = defaultUserInfo.length;
+
+    function adjustPinLayout() {
+        let maxWidth = userPin.offsetWidth;
+        let maxHeight = userPin.offsetHeight;
+
+        let width = 11;
+        let height = 8;
+        let f = Math.min(maxWidth / width, maxHeight / height);
+        width *= f;
+        height *= f;
+
+        // let margin = width * 0.01;
+        let margin = 0;
+
+        for (let userTag of userPin.children) {
+            userTag.style.width = width - 2 * margin + "px";
+            userTag.style.height = height - 2 * margin + "px";
+
+            userTag.style.top = (maxHeight - height) / 2 + margin + "px";
+            userTag.style.left = (maxWidth - width) / 2 + margin + "px";
+            // console.log(userTag);
+        }
+    }
+
+    function adjustListLayout() {
+        let maxWidth = userList.offsetWidth;
+        let maxHeight = userList.offsetHeight;
+
+        let width = 11;
+        let height = 8;
+        let f = 0;
+        let cols = 0;
+        let rows = 0;
+
+        for (let i = 1; i <= listUserCount; i++) {
+            let fullWidth = width * i;
+            let fullHeight = height * Math.ceil(listUserCount / i);
+
+            let widthF = maxWidth * 0.95 / fullWidth;
+            let heightF = maxHeight * 0.95 / fullHeight;
+            
+            let minF = Math.min(widthF, heightF);
+            if (minF > f) {
+                f = minF;
+                cols = i;
+                rows = Math.ceil(listUserCount / cols);
+            }
+        }
+
+        width *= f;
+        height *= f;
+        margin = width * 0.01;
+
+        let fullWidth = width * cols;
+        let fullHeight = height * rows;
+
+        let lastCols = cols - cols * rows + listUserCount;
+        let lastRowFull = (cols * rows == listUserCount);
+        let extF = Math.min(1.2, (fullWidth / lastCols) / width);
+        let lastWidth = lastCols * width * extF;
+
+        let baseTop = (maxHeight - fullHeight) / 2;
+        let baseLeft = (maxWidth - fullWidth) / 2;
+
+        let col = 0;
+        let row = 0;
+        for (let userTag of userList.children) {
+            if (!lastRowFull && row == rows - 1) {
+                userTag.style.width = (width * extF - 2 * margin) + "px";
+                userTag.style.height = (height - 2 * margin) + "px";
+
+                userTag.style.top = baseTop + row * height + margin + "px";
+                userTag.style.left = baseLeft + (fullWidth - lastWidth) / 2 + col * width * extF + margin + "px";
+            }
+            else {
+                userTag.style.width = (width - 2 * margin) + "px";
+                userTag.style.height = (height - 2 * margin) + "px";
+
+                userTag.style.top = baseTop + row * height + margin + "px";
+                userTag.style.left = baseLeft + col * width + margin + "px";
+            }
+
+            col += 1;
+            if (col == cols) {
+                col = 0;
+                row += 1;
+            }
+        }
+    }
+
+    function adjustLayout() {
+        adjustPinLayout();
+        adjustListLayout();
+    }
 
     for (let userInfo of defaultUserInfo) {
         let userTag = templateUser.cloneNode(true);
@@ -42,6 +138,9 @@ window.onload = () => {
                 userList.removeChild(userTag);
             }
             participantCountTag.innerText -= 1;
+            listUserCount -= 1;
+
+            adjustLayout();
         }
         deleteButton.addEventListener("click", deleteClickListener);
         // set tooltip event listener for clicking
@@ -55,6 +154,8 @@ window.onload = () => {
 
                 userTag.setAttribute("pinned", false);
                 userList.appendChild(userTag);
+
+                listUserCount += 1;
             }
             else {
                 userTag.setAttribute("pinned", true);
@@ -75,8 +176,12 @@ window.onload = () => {
 
                     userPin.setAttribute("enabled", true);
                     userPin.appendChild(userTag);
+
+                    listUserCount -= 1;
                 }
             }
+
+            adjustLayout();
         }
         for (let option of tooltipOptions) {
             switch (option.getAttribute("id")) {
@@ -93,7 +198,12 @@ window.onload = () => {
         }
         // append child
         userList.appendChild(userTag);
+        listUserCount += 1;
     }
+
+    window.addEventListener("resize", () => adjustLayout());
+
+    adjustLayout();
 }
 
 
